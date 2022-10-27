92c362bf51a31726219040fa70d9484458a1591d
Flippchen <91947480+flippchen@users.noreply.github.com>
2022-10-27 12:07:28 +0200

    Added main and dummys for pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,67 @@
+import 'package:connactivity/account_page.dart';
+import 'package:connactivity/feed_page.dart';
+import 'package:connactivity/my_page.dart';
+import 'package:connactivity/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//https://coolors.co/dcf763-02020a-fe7f2d-b497d6-52d1dc
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainConnActivity());
 }
+class MainConnActivity extends StatelessWidget {
+  const MainConnActivity({ Key? key }) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return  MaterialApp(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF02020A),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xFFFE7F2D)),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white)
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ConnActivityHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ConnActivityHome extends StatefulWidget {
+  const ConnActivityHome({ Key? key }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ConnActivityHome> createState() => _ConnActivityHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _ConnActivityHomeState extends State<ConnActivityHome> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xff02020A),
+          foregroundColor: const Color(0xff02020A),
+          title: Center(
+            child: Text("ConnActivity",
+            style: GoogleFonts.lobster(color: const Color(0xffFE7F2D), fontSize: 30)),
+          ),
         ),
+        backgroundColor: Colors.black,
+        body: TabBarView(children: [
+          LayoutBuilder(builder:(context, constraints) {
+            debugPrint(constraints.maxHeight.toString());
+            return FeedPage(height: constraints.maxHeight,);}),
+          //const Center(child: Text("👋 You have not entered any events yet", style: TextStyle(color: Colors.white),)),
+          const MyPAge(),
+          const AccountPage(),
+        ]),
+        bottomNavigationBar: const ConnActivityNavBar(),
+        
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
