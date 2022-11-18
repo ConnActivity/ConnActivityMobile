171f415a167dfdf31611a436359d8d54c9675fbe
Nereuxofficial <37740907+nereuxofficial@users.noreply.github.com>
2022-11-18 20:04:00 +0100

    Added nav_bar

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,1 +1,33 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class ConnActivityNavBar extends StatelessWidget {
+  const ConnActivityNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.white70,
+      indicatorSize: TabBarIndicatorSize.tab,
+      indicatorPadding: const EdgeInsets.all(5.0),
+      indicatorColor: const Color(0xffFE7F2D),
+      indicator: BoxDecoration(
+          color: const Color(0xffFE7F2D),
+          borderRadius: BorderRadius.circular(50)),
+      tabs: const [
+        Tab(
+          text: "Feed",
+          icon: Icon(Icons.feed),
+        ),
+        Tab(
+          text: "My",
+          icon: Icon(Icons.event),
+        ),
+        Tab(
+          text: "Account",
+          icon: Icon(Icons.person),
+        )
+      ],
+    );
+  }
+}
