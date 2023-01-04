08cbe4cc53fed6a0d4a06549942eb7993e7c85a4
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-01-05 00:37:13 +0100

    fmt

--- lib/comms.dart
+++ lib/comms.dart
@@ -23,22 +23,24 @@ Future<bool> joinEvent(int id) async {
 
 Future<bool> leaveEvent(int id) async {
   var userToken = await getUserToken();
- //var event = await http
- //    .get(Uri.parse("https://api.connactivity.me/events/$id"), headers: {
- //  "cookie": "user_token=$userToken",
- //});
- //var jsnondecoded = json.decode(event.body);
- //var creator = jsnondecoded["creator"];
+  //var event = await http
+  //    .get(Uri.parse("https://api.connactivity.me/events/$id"), headers: {
+  //  "cookie": "user_token=$userToken",
+  //});
+  //var jsnondecoded = json.decode(event.body);
+  //var creator = jsnondecoded["creator"];
   var response = await http
       .put(Uri.parse("https://api.connactivity.me/leave_event/$id"), headers: {
     "cookie": "user_token=$userToken",
   });
-  print("----------------------------------------------------------------------------------");
+  print(
+      "----------------------------------------------------------------------------------");
   print(id.toString());
   print(response.statusCode);
   //print(event.statusCode);
   //print(event.body.toString());
-  print("----------------------------------------------------------------------------------");
+  print(
+      "----------------------------------------------------------------------------------");
 
   return response.statusCode == 200;
 }
