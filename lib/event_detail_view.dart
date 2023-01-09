d0c8b5265d275b360e6789560d2eb4cef525de07
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-01-09 22:52:45 +0100

    dart fmt

--- lib/event_detail_view.dart
+++ lib/event_detail_view.dart
@@ -36,135 +36,140 @@ class DetailScreenState extends State<DetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: widget.height,
-      margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: widget.backgroundColor),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 320,
-            height: 200,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: (widget.feedElementData.image.isEmpty)
-                  ? const Icon(Icons.image)
-                  : Image.memory(widget.feedElementData.image),
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
+        height: widget.height,
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: widget.backgroundColor),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 320,
+              height: 200,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: (widget.feedElementData.image.isEmpty)
+                    ? const Icon(Icons.image)
+                    : Image.memory(widget.feedElementData.image),
+              ),
             ),
-          ),
-          Text(widget.feedElementData.title,
-              style: GoogleFonts.anton(
-                  textStyle:
-                      const TextStyle(color: Colors.black, fontSize: 30))),
-          Row(
-            children: [
-              const Icon(Icons.place_outlined),
-              Text(widget.feedElementData.place ?? "No location data",
-                  style: const TextStyle(color: Colors.black, fontSize: 20)),
-            ],
-          ),
-          Row(
-            children: [
-              const Icon(Icons.schedule),
-              Text(
-                  widget.feedElementData.time != null
-                      ? timeToString(widget.feedElementData.time!.toLocal())
-                      : "No date data",
-                  style: const TextStyle(color: Colors.black, fontSize: 20)),
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Text(widget.feedElementData.title,
+                style: GoogleFonts.anton(
+                    textStyle:
+                        const TextStyle(color: Colors.black, fontSize: 30))),
+            Row(
               children: [
-                //const Icon(Icons.info_outline),
-                Flexible(
-                    child: Text(
-                  widget.feedElementData.description,
-                  style: const TextStyle(
-                      fontSize: 20, fontStyle: FontStyle.italic),
-                )),
+                const Icon(Icons.place_outlined),
+                Text(widget.feedElementData.place ?? "No location data",
+                    style: const TextStyle(color: Colors.black, fontSize: 20)),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(0, 15, 5, 0),
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      bool response = joint;
-                      if (!joint) {
-                        response = await joinEvent(widget.feedElementData.id);
-                      } else {
-                        response = await leaveEvent(widget.feedElementData.id);
-                        response = !response;
-                      }
-                      setState(() {
-                        joint = response;
-                      });
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: joint
-                            ? MaterialStateProperty.all(Colors.black)
-                            : MaterialStateProperty.all(
-                                const Color(0xffFE7F2D)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ))),
-                    child: joint
-                        ? const Icon(
-                            Icons.check,
-                            color: Colors.white,
-                          )
-                        : const Text(
-                            "Join",
-                            style: TextStyle(color: Colors.black, fontSize: 20),
-                          ),
-                  ),
-                ),
+            Row(
+              children: [
+                const Icon(Icons.schedule),
+                Text(
+                    widget.feedElementData.time != null
+                        ? timeToString(widget.feedElementData.time!.toLocal())
+                        : "No date data",
+                    style: const TextStyle(color: Colors.black, fontSize: 20)),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //const Icon(Icons.info_outline),
+                  Flexible(
+                      child: Text(
+                    widget.feedElementData.description,
+                    style: const TextStyle(
+                        fontSize: 20, fontStyle: FontStyle.italic),
+                  )),
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  width: double.infinity,
-                  margin: const EdgeInsets.fromLTRB(5, 15, 0, 0),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        shadowColor:
-                            MaterialStateProperty.all(Colors.transparent),
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.transparent),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                          side: const BorderSide(
-                              color: Color(0xffFE7F2D), width: 2),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ))),
-                    child: const Text(
-                      "Details",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(0, 15, 5, 0),
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        bool response = joint;
+                        if (!joint) {
+                          response = await joinEvent(widget.feedElementData.id);
+                        } else {
+                          response =
+                              await leaveEvent(widget.feedElementData.id);
+                          response = !response;
+                        }
+                        setState(() {
+                          joint = response;
+                        });
+                      },
+                      style: ButtonStyle(
+                          backgroundColor: joint
+                              ? MaterialStateProperty.all(Colors.black)
+                              : MaterialStateProperty.all(
+                                  const Color(0xffFE7F2D)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ))),
+                      child: joint
+                          ? const Icon(
+                              Icons.check,
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              "Join",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 20),
+                            ),
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
-        ],
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.fromLTRB(5, 15, 0, 0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shadowColor:
+                              MaterialStateProperty.all(Colors.transparent),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.transparent),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            side: const BorderSide(
+                                color: Color(0xffFE7F2D), width: 2),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ))),
+                      child: const Text(
+                        "Details",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
