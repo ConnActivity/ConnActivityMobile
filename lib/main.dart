5455351aee4c1aa26b67cf7244f5740da73b7f94
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-01-20 22:51:35 +0100

    first impl. of welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:connactivity/account_page.dart';
 import 'package:connactivity/feed_page.dart';
+import 'package:connactivity/introduction_screen.dart';
 import 'package:connactivity/my_page.dart';
 import 'package:connactivity/nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 //https://coolors.co/dcf763-02020a-fe7f2d-b497d6-52d1dc
 
@@ -36,34 +38,50 @@ class ConnActivityHome extends StatefulWidget {
 }
 
 class _ConnActivityHomeState extends State<ConnActivityHome> {
+  var _isInit = false;
+  @override
+  initState() {
+    super.initState();
+    helper();
+  }
+
+  helper() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isInit = prefs.getBool("isInit") ?? false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xff02020A),
-          foregroundColor: const Color(0xff02020A),
-          title: Center(
-            child: Text("ConnActivity",
-                style: GoogleFonts.lobster(
-                    color: const Color(0xffFE7F2D), fontSize: 30)),
-          ),
-        ),
-        backgroundColor: Colors.black,
-        body: TabBarView(children: [
-          LayoutBuilder(builder: (context, constraints) {
-            debugPrint(constraints.maxHeight.toString());
-            return FeedPage(
-              height: constraints.maxHeight,
-            );
-          }),
-          //const Center(child: Text("👋 You have not entered any events yet", style: TextStyle(color: Colors.white),)),
-          const MyPAge(),
-          const AccountPage(),
-        ]),
-        bottomNavigationBar: const ConnActivityNavBar(),
-      ),
-    );
+    return _isInit == true
+        ? DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: const Color(0xff02020A),
+                foregroundColor: const Color(0xff02020A),
+                title: Center(
+                  child: Text("ConnActivity",
+                      style: GoogleFonts.lobster(
+                          color: const Color(0xffFE7F2D), fontSize: 30)),
+                ),
+              ),
+              backgroundColor: Colors.black,
+              body: TabBarView(children: [
+                LayoutBuilder(builder: (context, constraints) {
+                  debugPrint(constraints.maxHeight.toString());
+                  return FeedPage(
+                    height: constraints.maxHeight,
+                  );
+                }),
+                //const Center(child: Text("👋 You have not entered any events yet", style: TextStyle(color: Colors.white),)),
+                const MyPAge(),
+                const AccountPage(),
+              ]),
+              bottomNavigationBar: const ConnActivityNavBar(),
+            ),
+          )
+        : const IntroductionScreens();
   }
 }
